d791d8e5a346f389b466fb422aec529402c85851
IlliaVoronov <iv@nemesiscorporation.com>
2024-08-06 14:00:26 +0300

    add fade animation to darken bg

--- Assets/Runtime/Window/WindowParaLayer.cs
+++ Assets/Runtime/Window/WindowParaLayer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using System.Collections.Generic;
 
 namespace deVoid.UIFramework {
@@ -7,10 +8,16 @@ namespace deVoid.UIFramework {
     /// By default, it contains any window tagged as a Popup. It is controlled by the WindowUILayer.
     /// </summary>
     public class WindowParaLayer : MonoBehaviour {
-        [SerializeField] 
-        private GameObject darkenBgObject = null;
+        [SerializeField] private GameObject darkenBgObject = null;
+        [SerializeField] private float _appearDarkenBgDuration = 0.4f;
+        private CanvasGroup _darkenBgCanvasGroup;
+        private Coroutine _activeRoutine;
 
         private List<GameObject> containedScreens = new List<GameObject>();
+        
+        private void Awake() {
+            EnsureCanvasGroupExist();
+        }
         
         public void AddScreen(Transform screenRectTransform) {
             screenRectTransform.SetParent(transform, false);
@@ -21,19 +28,68 @@ namespace deVoid.UIFramework {
             for (int i = 0; i < containedScreens.Count; i++) {
                 if (containedScreens[i] != null) {
                     if (containedScreens[i].activeSelf) {
-                        darkenBgObject.SetActive(true);
-                        darkenBgObject.transform.SetAsFirstSibling();
+                        ShowDarkenBg();
                         return;
                     }
                 }
             }
 
-            darkenBgObject.SetActive(false);
+            HideDarkenBg();
         }
 
-        public void DarkenBG() {
+        public void DarkenBG() => ShowDarkenBg();
+        
+        private void ShowDarkenBg() {
+            StopActiveDarkenBgRoutine();
             darkenBgObject.SetActive(true);
-            darkenBgObject.transform.SetAsLastSibling();
+            darkenBgObject.transform.SetAsFirstSibling();
+            _activeRoutine = StartCoroutine(FadeInDarkenBgCoroutine());
         }
+
+        private void HideDarkenBg() {
+            StopActiveDarkenBgRoutine();
+            _activeRoutine = StartCoroutine(FadeOutDarkenBgCoroutine());
+        }
+        
+        private void EnsureCanvasGroupExist() {
+            _darkenBgCanvasGroup = darkenBgObject.GetComponent<CanvasGroup>();
+            if (_darkenBgCanvasGroup == null) {
+                _darkenBgCanvasGroup = darkenBgObject.AddComponent<CanvasGroup>();
+            }
+        }
+        
+        private void StopActiveDarkenBgRoutine() {
+            if (_activeRoutine != null) {
+                StopCoroutine(_activeRoutine);
+            }
+        }
+        
+        private IEnumerator FadeInDarkenBgCoroutine() {
+            float t = Time.deltaTime;
+            SetDarkenBgAlpha(0);
+            while (t < _appearDarkenBgDuration) {
+                SetDarkenBgAlpha(t / _appearDarkenBgDuration);
+                _darkenBgCanvasGroup.alpha = t / _appearDarkenBgDuration;
+                yield return new WaitForEndOfFrame();
+                t += Time.deltaTime;
+            }
+
+            SetDarkenBgAlpha(1);
+        }
+        
+        private IEnumerator FadeOutDarkenBgCoroutine() {
+            float t = Time.deltaTime;
+            SetDarkenBgAlpha(1);
+            while (t < _appearDarkenBgDuration) {
+                SetDarkenBgAlpha(1f - t / _appearDarkenBgDuration);
+                yield return new WaitForEndOfFrame();
+                t += Time.deltaTime;
+            }
+
+            SetDarkenBgAlpha(0);
+            darkenBgObject.SetActive(false);
+        }
+        
+        private void SetDarkenBgAlpha(float a) => _darkenBgCanvasGroup.alpha = a;
     }
 }
